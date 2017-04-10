495eae5b2de3893af68d98298a96d253c4ec10f7
hoanganhtuanbk <anhtuanhnh@gmail.com>
2017-04-10 15:26:25 +0700

    validate upload

--- client/components/layouts/PostWidget/PostWidget.js
+++ client/components/layouts/PostWidget/PostWidget.js
@@ -8,6 +8,9 @@ export class PostCreateWidget extends Component {
     super();
     this.state = {
       imgSrc: '',
+      showError : false,
+      showErrorSize : false,
+      disabled: true
     };
   }
   addPost = () => {
@@ -24,6 +27,12 @@ export class PostCreateWidget extends Component {
     e.preventDefault();
     const reader = new FileReader();
     const file = e.target.files[0];
+    console.log(e.target.files[0].size);
+    if(e.target.files[0].size > 4000000){
+      this.setState({showErrorSize : true})
+    } else {
+      this.setState({showErrorSize : false})
+    }
     reader.onloadend = () => {
       this.setState({
         file,
@@ -32,14 +41,35 @@ export class PostCreateWidget extends Component {
     };
     reader.readAsDataURL(file);
   }
+  handleChange(e){
+    console.log(e);
+    if(e.target.value.length == 0){
+      this.setState({showError: true});
+    } else {
+      this.setState({showError: false, disabled: false});
+    }
+}
+  handleBlur(e){
+    if(e.target.value.length == 0){
+      this.setState({showError: true});
+    }else {
+      this.setState({showError: false, disabled: false});
+    }
+  }
   render() {
     const cls = `${styles.form} ${(this.props.showElement === 'post' ? styles.appear : '')}`;
     return (
       <div className={cls}>
         <div className={styles['form-content']}>
           <h2 className={styles['form-title']}>Đăng bài mới</h2>
-          <input placeholder="Tiêu đề" className={styles['form-field']} ref="title" onChange={this.checkStatusLimit} />
+          <input placeholder="Tiêu đề" className={styles['form-field']} style={this.state.showError ? {border: '1px solid red'}: null} ref="title" onChange={(e) => this.handleBlur(e)} />
+          {
+            this.state.showError ? <span>Tiêu đề không được trống</span> : null
+          }
           <input type="file" ref="file" onChange={(e) => this.loadFile(e)} />
+          {
+            this.state.showErrorSize ? <span>Kích thước file không được quá 5Mb</span> : null
+          }
           <select ref="category" className={styles.select}>
             <option value="GIF">GIF</option>
             <option value="Comic">Comic</option>
@@ -50,11 +80,13 @@ export class PostCreateWidget extends Component {
             <option value="Meme">Meme</option>
             <option value="WTF">WTF</option>
           </select>
-          <Link to={this.state.imgSrc} target="_blank">
-            <img src={this.state.imgSrc} alt="" />
-          </Link>
+          {
+            this.state.showErrorSize ? null : <Link to={this.state.imgSrc} target="_blank" >
+                <img src={this.state.imgSrc} alt="" />
+              </Link>
+          }
           <a className={styles['post-submit-button']} onClick={this.props.closeElement}>Cancel</a>
-          <a className={styles['post-submit-button']} onClick={this.addPost}>Submit</a>
+          <a className={styles['post-submit-button']} style={this.state.showError || this.state.showErrorSize ? {background: 'rgba(89, 120, 54, 0.53)'}: null} onClick={this.addPost} disabled={this.state.showError || this.state.showErrorSize || this.state.disabled}>Submit</a>
         </div>
         <div className={styles.backgroundPost} onClick={this.props.closeElement}>
         </div>
