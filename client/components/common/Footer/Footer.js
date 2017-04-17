1b10ef2b24e0816e318385a1578ac0c920271d05
Hieu.tt <trunghieutk459@gmail.com>
2017-04-17 14:45:45 +0700

    fix ui

--- client/components/common/Footer/Footer.js
+++ client/components/common/Footer/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
     <footer className={styles['main-footer']}>
       <div className="container">
         <div className={styles['pull-middle']}>
-          <a href="about_us.html">About Us</a> | <a href="privacy_policy.html">Privacy Policy</a> | <a
-            href="tos.html"
-          >Terms of Use</a> | <a href="dmca.html">DMCA</a> | <a href="rss.html">Rss Feeds</a>|
-          <a href="contact_us.html">Contact Us</a>
+          <a href="">Về chúng tôi</a> | <a href="">Chính sách</a> | <a
+          href=""
+        >Điều luật</a> |
+          <a href=""> Liên hệ</a>
         </div>
         <div className={`${styles['pull-middle']} ${styles['copyright-note']}`}>&#169; 2017 Tuổi học trò. All Rights
           Reserved.
